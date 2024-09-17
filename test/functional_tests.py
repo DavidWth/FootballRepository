2774ce69e9e3087330d9f888020467b84c82f369
David <dw@yahoo.de>
2024-09-17 11:02:04 +0200

    Added functionality to functional test class

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -1,7 +1,19 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://www.google.de")
+class KickerTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self) -> None:
+        self.browser.quit()
+    
+    def test_get_matchday(self):
+        self.browser.get("http://www.google.com")
 
-assert "Google" in browser.title
-print("OK")
+        self.assertIn("Google", self.browser.title)
+
+        self.fail("Finish the test!")
+    
+if __name__ == "__main__":
+    unittest.main()
